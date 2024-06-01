e22212b31a0d3808f6bad016cc94ecb57b036da5
PRIYANSHU KUMAR RAJ <priyanshuraj2552@gmail.com>
2024-06-02 00:08:47 +0530

    added admin.js adminloginauth and modified flights.js

--- server/router/route.js
+++ server/router/route.js
@@ -1,24 +1,24 @@
 // backend/routes/flights.js
 const express = require('express');
 const router = express.Router();
-const { searchFlight, bookFlight, getBookedFlights, getUserDetails } = require('../controllers/appController');
-const { addFlight, editFlight, deleteFlight, } = require('../controllers/adminControllers');
+const { searchFlight, bookFlight, getBookedFlights, getUserDetails, updateUserDetails } = require('../controllers/appController');
+const { addFlight, editFlight, deleteFlight } = require('../controllers/flightController');
+const { verifyAdminToken } = require('../middleware/authMiddleware');
 
 /* POST Methods */
-router.route('/addflight').post(addFlight)
-router.route('/bookflight').post(bookFlight)
-router.route('/searchflight').post(searchFlight)
-
+router.route('/addflight').post(verifyAdminToken, addFlight);
+router.route('/bookflight').post(bookFlight);
+router.route('/searchflight').post(searchFlight);
 
 /* GET Methods */
-router.route('/getbookedflights').get(getBookedFlights)
-router.route('/getuserdetails').get(getUserDetails)
+router.route('/getbookedflights').get(getBookedFlights);
+router.route('/getuserdetails').get(getUserDetails);
 
-/**  PUT Methods */
-router.route('/updateuserdetails').put(updateUserDetails)
-router.route('/updateFlight/:id').put(editFlight)
+/* PUT Methods */
+router.route('/updateuserdetails').put(updateUserDetails);
+router.route('/updateFlight/:id').put(verifyAdminToken, editFlight);
 
-/**DELETE Methods */
-router.route('/deleteFlight/:id').delete(deleteFlight)
+/* DELETE Methods */
+router.route('/deleteFlight/:id').delete(verifyAdminToken, deleteFlight);
 
 module.exports = router;
